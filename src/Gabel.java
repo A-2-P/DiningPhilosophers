fe80fb4f34e106d363672a125211034accedbed9
Anel <42124062+A-2-P@users.noreply.github.com>
2020-07-24 17:07:40 +0200

    Update Gabel.java

--- src/Gabel.java
+++ src/Gabel.java
@@ -1,6 +1,6 @@
 import java.util.concurrent.locks.ReentrantLock;
 
-public class Gabel extends ReentrantLock { //reentrant  -> warte bis frei ist  - Gabeln sind LOCKS CHU CHUUU
+public class Gabel extends ReentrantLock { 
 
 
 }
